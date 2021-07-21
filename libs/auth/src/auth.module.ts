13aef71670c7e6d8b7f3da15468a13274a00eb8d
cyyjs <cyyjs@cyyjs.top>
2021-07-21 15:50:31 +0800

    update

--- libs/auth/src/auth.module.ts
+++ libs/auth/src/auth.module.ts
@@ -1,4 +1,5 @@
-import { DynamicModule, HttpModule, Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
+import { HttpModule} from '@nestjs/axios'
 import { PassportModule } from '@nestjs/passport';
 import { AUTH_MODULE_OPTIONS } from './auth.contents';
 import { AuthModuleOptions } from './auth.interfaces';
